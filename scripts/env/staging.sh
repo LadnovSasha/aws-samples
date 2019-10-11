f2f76868e823cf1fc311b58137ef094fe487b2ce
Volodymyr Oshchypko <voshchypko@intellias.com>
2019-10-11 07:38:25 +0000

    Merged in feature/DOE-573 (pull request #5)
    [DOE-573] Envs names changed
    
    * [DOE-573] Envs names changed
    
    * [DOE-573] Staging and prod aws credentials changed

--- scripts/env/staging.sh
+++ scripts/env/staging.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-access_key_id="${bamboo_aws_gaas_access_key_id}"
-secret_access_key="${bamboo_aws_gaas_secret_access_key}"
+access_key_id="${bamboo_aws_gaas_staging_access_key_id}"
+secret_access_key="${bamboo_aws_gaas_staging_secret_access_key}"
 aws_region="${bamboo_aws_default_region}"
 
-pg_user="${bamboo_lead_time_lambda_pg_user}"
-pg_password="${bamboo_lead_time_lambda_pg_password}"
+pg_user="${bamboo_fitment_lambda_staging_pg_user}"
+pg_password="${bamboo_fitment_lambda_staging_pg_password}"
 
-serverless_salt="${bamboo_lead_time_lambda_test_salt}"
-vpcsecuritygroup="${bamboo_fitment_lambda_test_vpcsecuritygroup}"
-vpcsubnet="${bamboo_fitment_lambda_test_vpcsubnet}"
+serverless_salt="${bamboo_fitment_lambda_staging_salt}"
+vpcsecuritygroup="${bamboo_fitment_lambda_staging_vpcsecuritygroup}"
+vpcsubnet="${bamboo_fitment_lambda_staging_vpcsubnet}"
--- scripts/env/test.sh
+++ scripts/env/test.sh
@@ -6,9 +6,9 @@ access_key_id="${bamboo_aws_gaas_access_key_id}"
 secret_access_key="${bamboo_aws_gaas_secret_access_key}"
 aws_region="${bamboo_aws_default_region}"
 
-pg_user="${bamboo_lead_time_lambda_pg_user}"
-pg_password="${bamboo_lead_time_lambda_pg_password}"
+pg_user="${bamboo_fitment_lambda_test_pg_user}"
+pg_password="${bamboo_fitment_lambda_test_pg_password}"
 
-serverless_salt="${bamboo_lead_time_lambda_test_salt}"
+serverless_salt="${bamboo_fitment_lambda_test_salt}"
 vpcsecuritygroup="${bamboo_fitment_lambda_test_vpcsecuritygroup}"
 vpcsubnet="${bamboo_fitment_lambda_test_vpcsubnet}"
